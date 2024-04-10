83c3029892a760120c23792853eee7b71b1a1a92
Tathata <andre.yovera1@gmail.com>
2024-04-10 15:38:12 -0400

    Building a Peloton Clone with Xamarin.Forms: Part 3 - Refactoring & CarouselView

---
+++ src/FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessApp.ViewModels
+{
+    public class ChallengesViewModel : BaseViewModel
+    {
+        private int selectedCategoryIndex;
+        public int SelectedCategoryIndex { get => selectedCategoryIndex; set => SetProperty(ref selectedCategoryIndex, value); }
+
+        public ChallengesViewModel()
+        {
+            SelectCategoryCommand = new Command<string>(OnSelectCategory);
+        }
+
+        void OnSelectCategory(string index)
+        {
+            if (int.TryParse(index, out int i))
+            {
+                SelectedCategoryIndex = i;
+            }
+        }
+        public Command<string> SelectCategoryCommand { get; }
+    }
+}
---
+++ src/FitnessApp/ViewModels/FeaturedViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace FitnessApp.ViewModels
+{
+    public class  FeaturedViewModel : BaseViewModel
+    {
+    }
+}
